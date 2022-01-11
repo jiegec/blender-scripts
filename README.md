07c0a0fcf5bc614a12acb6505a37e9d88e0eea5f
Jiajie Chen <c@jia.je>
2022-01-11 12:01:14 +0800

    Add README and rename

---
+++ README.md
@@ -0,0 +1,8 @@
+# Blender Scripts
+
+## Bake vertex colors to texture image
+
+- Run with: blender -b -P bake_vertex_colors_to_texture_image.py -- input_ply output_obj
+- Tested with blender 3.0.0
+- Input: PLY format with vertex color attributes i.e. rgb channels
+- Output: OBJ format with texture image
--- bake_vertex_colors_to_texture_image.py
+++ bake_vertex_colors_to_texture_image.py
@@ -1,5 +1,8 @@
+# Bake vertex colors to texture image
+# Run with: blender -b -P bake_vertex_colors_to_texture_image.py -- input_ply output_obj
 # Tested with blender 3.0.0
-# Run with: blender -b -P texture.py -- input_ply output_obj
+# Input: PLY format with vertex color attributes i.e. rgb channels
+# Output: OBJ format with texture image
 
 import bpy
 import argparse
